26115acb6be54288e4677afac1c9cf65719b3719
unknown <juan.ocabo@gmail.com>
2024-02-13 11:40:07 +0100

    reto2

---
+++ myonlineshop/src/main/java/com/myonlineshop/myonlineshop/controller/AccountsController.java
@@ -0,0 +1,12 @@
+package com.myonlineshop.myonlineshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/accounts")
+public class AccountsController {
+
+}
