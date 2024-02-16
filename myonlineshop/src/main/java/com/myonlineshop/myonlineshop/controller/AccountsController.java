d28f08ffe6cb8be50ee2c805ffbde9fea3147fea
unknown <raquel.llorente20@gmail.com>
2024-02-16 14:21:28 +0100

    update

--- myonlineshop/src/main/java/com/myonlineshop/myonlineshop/controller/AccountsController.java
+++ myonlineshop/src/main/java/com/myonlineshop/myonlineshop/controller/AccountsController.java
@@ -2,14 +2,17 @@ package com.myonlineshop.myonlineshop.controller;
 
 import com.myonlineshop.myonlineshop.exception.AccountNotfoundException;
 import com.myonlineshop.myonlineshop.model.Account;
-import com.myonlineshop.myonlineshop.model.Customer;
 import com.myonlineshop.myonlineshop.persistence.AccountRepository;
 import com.myonlineshop.myonlineshop.services.AccountService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.Valid;
@@ -24,12 +27,19 @@ import java.util.List;
 @RestController
 @RequestMapping("/accounts")
 @Validated
+@Tag(name = "API de cuentas", description = "Endpoints para consumir cuentas")
 public class AccountsController {
 
     @Autowired
     AccountService accountService;
     @Autowired
     AccountRepository accountRepository;
+
+    @Operation(summary = "Para listar todas las cuentas", description = "Devuelve todas las cuentas")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Cuando hay cuentas a devolver."),
+        @ApiResponse(responseCode = "404", description = "Cuando no hay cuentas a devolver."),
+    })
     @GetMapping(value = "", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<Object> listar(){
         List<Account> accs = accountService.getAccounts();
@@ -42,6 +52,11 @@ public class AccountsController {
         );
     }
 
+    @Operation(summary = "Para crear una cuenta nueva", description = "Crea una cuenta nueva a partir del JSON/XML")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Cuando hay cuentas a devolver."),
+        @ApiResponse(responseCode = "412", description = "Cuando el JSON/XML está mal formateado."),
+    })
     @PostMapping(value = "", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<Account> create(@RequestBody @Valid Account newAcc) {
         return new ResponseEntity<>(accountService.create(newAcc), HttpStatus.CREATED);
@@ -58,8 +73,15 @@ public class AccountsController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Para añadir cantidad al balance", description = "Añade una cantidad al balance de la cuenta de un cliente")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Cuando hay cuentas a devolver."),
+        @ApiResponse(responseCode = "412", description = "Cuando el JSON/XML está mal formateado."),
+    })
     @PutMapping("/addBalance/{id}")
-    public ResponseEntity addBalance(@PathVariable("id") @Min(1) Long id,@RequestParam Long cid,@RequestParam int mount){
+    public ResponseEntity addBalance(@PathVariable("id") @Min(1) Long id,
+                                     @Parameter(name = "Id cliente", description = "Indica el identificador del cliente", example = "2") @RequestParam Long cid,
+                                     @Parameter(name = "Cantidad", description = "Indica la cantidad a añadir al balance", example = "50") @RequestParam int mount){
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(accountService.addBalance(id, mount,cid));
     }
     @PutMapping("/withdrawBalance/{id}")
--- myonlineshop/src/main/java/com/myonlineshop/myonlineshop/model/Account.java
+++ myonlineshop/src/main/java/com/myonlineshop/myonlineshop/model/Account.java
@@ -9,7 +9,11 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
 import java.util.Date;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
 
 @Getter
 @Setter
@@ -17,18 +21,23 @@ import java.util.Date;
 @AllArgsConstructor
 @Entity
 @Table(name = "accounts")
+@XmlRootElement
+@Schema(name = "Modelo de cuenta", description = "Representa una cuenta de un cliente")
 public class Account {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Min(1)
+    @Schema(name = "Id cuenta", description = "Identificador de la cuenta basado en números enteros positivos", example = "12")
     private Long id;
 
     @NotBlank
     @Size(max = 20)
+    @Schema(name = "Tipo de cuenta", description = "Tipo de la cuenta en caracters alfanuméricos", example = "Tipo1")
     private String type;
 
     @DateTimeFormat
+    @Schema(name = "Fecha apertura cuenta", description = "Fecha y hora en la que se ha abierto la cuenta", example = "2024-02-16T12:37:56.401+00:00")
     Date openingDate;
 
     private int balance;
